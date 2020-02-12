410283e637a898e841e8fe8e26c59e2a0194a1ae
yuribol <yuri.csf+pb@gmail.com>
2020-02-12 09:28:01 -0500

    Trying proxying to local

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,7 @@
-from flask import render_template
+import os
+import requests
+
+from flask import render_template, make_response, render_template_string
 
 from app import app
 
@@ -10,3 +13,13 @@ def index():
     user = {'username': 'Bonanza'}
 
     return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/proxy/<str:short_url>')
+def proxy_to_local(short_url):
+
+    local_ngrok_url = os.environ.get('LOCAL_NGROK_URL')
+
+    response = requests.get(local_ngrok_url + "/" + short_url)
+
+    return render_template_string(response.text)
